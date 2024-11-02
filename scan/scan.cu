b13890f7689191ca745142aafb139df1ea7bfc22
Ishita Gupta <ishig@myth54.stanford.edu>
2024-11-02 11:23:40 -0700

    added helper fucntions

--- scan/scan.cu
+++ scan/scan.cu
@@ -214,8 +214,8 @@ int find_repeats(int* device_input, int length, int* device_output) {
     return 0; 
 }
 
-__global__ void pair_and_compare(int* input_data, bool* positions_mask, int length)
-{
+
+__global__ void pair_and_compare(int* input_data, bool* positions_mask, int length){
     int idx = blockIdx.x * blockDim.x + threadIdx.x; //establish thread identity 
     if (idx >= length - 1) return; //is it possible that the input_data size is smalled than the length? 
     if (input_data[idx] == input_data[idx + 1]) {
@@ -224,9 +224,19 @@ __global__ void pair_and_compare(int* input_data, bool* positions_mask, int leng
     else {
         positions_mask[idx] = 0;
     }
-
-
 }
+
+__global__ void cuda_identify_transition_points(int* prefix_sum_array, int* result, int length) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x; // Unique thread index
+
+    if (idx >= length - 1) return; // Ensure we stay within bounds for idx + 1
+
+    // Check if we have a transition point
+    if (prefix_sum_array[idx + 1] == prefix_sum_array[idx] + 1) {
+        result[prefix_sum_array[idx]] = idx;
+    }
+}
+
 
 
 //
